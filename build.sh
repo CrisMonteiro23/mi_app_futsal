f6e3566e6fce80a8b042d4497cab617ca52cf58a
CrisMonteiro23 <cristianjmonteiro@gmail.com>
2025-07-29 10:48:49 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-curl -sL https://storage.googleapis.com/flutter_infra_release/releases/master/linux/flutter_linux_master.tar.xz | tar xJ
+# Descargar Flutter beta 3.35.0-0.1.pre (incluye Dart 3.9.0)
+curl -sL https://storage.googleapis.com/flutter_infra_release/releases/beta/linux/flutter_linux_3.35.0-0.1.pre-beta.tar.xz | tar xJ
 git config --global --add safe.directory "$(pwd)/flutter"
 PATH="$(pwd)/flutter/bin:$PATH" flutter pub get
 PATH="$(pwd)/flutter/bin:$PATH" flutter build web --release
