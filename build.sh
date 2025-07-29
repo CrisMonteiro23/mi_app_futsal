faa2b1adb63703997dab4043997c5350b62cdfaf
CrisMonteiro23 <cristianjmonteiro@gmail.com>
2025-07-29 10:44:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -sL https://storage.googleapis.com/flutter_infra_release/releases/beta/linux/flutter_linux_3.22.0-0.2.pre-beta.tar.xz | tar xJ
+curl -sL https://storage.googleapis.com/flutter_infra_release/releases/master/linux/flutter_linux_master.tar.xz | tar xJ
 git config --global --add safe.directory "$(pwd)/flutter"
 PATH="$(pwd)/flutter/bin:$PATH" flutter pub get
 PATH="$(pwd)/flutter/bin:$PATH" flutter build web --release
