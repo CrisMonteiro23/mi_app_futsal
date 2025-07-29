b2452f2075fa6e5b28a2be0a315b290f9c00182d
CrisMonteiro23 <cristianjmonteiro@gmail.com>
2025-07-29 10:35:46 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.32.8-stable.tar.xz | tar xJ
+curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.1-stable.tar.xz | tar xJ
 git config --global --add safe.directory "$(pwd)/flutter"
 PATH="$(pwd)/flutter/bin:$PATH" flutter pub get
 PATH="$(pwd)/flutter/bin:$PATH" flutter build web --release
-# 6. Compilar la aplicación Flutter para la plataforma web en modo de lanzamiento.
-#    --release: Optimiza la aplicación para producción, reduciendo el tamaño y mejorando el rendimiento.
-flutter build web --release
