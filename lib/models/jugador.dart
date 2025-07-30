c7b1448533378023e4015c3b37ed9ee4d56de6bd
CrisMonteiro23 <cristianjmonteiro@gmail.com>
2025-07-30 12:53:28 +0200

    Update jugador.dart

--- lib/models/jugador.dart
+++ lib/models/jugador.dart
@@ -1,29 +1,9 @@
-// lib/models/jugador.dart
-// Este archivo define la estructura de datos para un Jugador.
-
 class Jugador {
-  String id; // Identificador único del jugador
-  String nombre; // Nombre completo del jugador (ej: "Victor")
+  final String id;
+  final String nombre;
 
-  // Constructor de la clase Jugador.
   Jugador({
     required this.id,
     required this.nombre,
   });
-
-  // Método para convertir un objeto Jugador a un formato JSON (Map).
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'nombre': nombre,
-    };
-  }
-
-  // Método de fábrica para crear un objeto Jugador desde un formato JSON (Map).
-  factory Jugador.fromJson(Map<String, dynamic> json) {
-    return Jugador(
-      id: json['id'],
-      nombre: json['nombre'],
-    );
-  }
 }
