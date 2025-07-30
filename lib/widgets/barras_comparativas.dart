cfc9940c58a4ea15156394dba5727957ab75c30b
CrisMonteiro23 <cristianjmonteiro@gmail.com>
2025-07-30 12:54:41 +0200

    Create barras_comparativas.dart

---
+++ lib/widgets/barras_comparativas.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BarrasComparativas extends StatelessWidget {
+  const BarrasComparativas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Gráfico en construcción'),
+    );
+  }
+}
